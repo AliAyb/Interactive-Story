767b0823577eb2436e5c8eeed8b373c6f2aa0db2
Mohammad-Agha <hamadaagha123@gmail.com>
2020-10-06 23:27:49 +0300

    Configured Reset Button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 const mainContainer = document.querySelector("#main-container");
 
+const resetButton = document.querySelector(".reset-game");
+
 function slideAnimation() {
     mainContainer.style.animationName = "sliding";
     setTimeout(() => {
         mainContainer.style.animationName = "";
     }, 1000);
 }
+
+resetButton.addEventListener("click", () => {
+    if (confirm("Are you sure you want to reset the game ?")) {
+        location.href = "/";
+    }
+})
