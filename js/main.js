fd2699aff51d0fee8d97c51d4f7206024fdb7541
Mohammad-Agha <hamadaagha123@gmail.com>
2020-10-06 22:47:57 +0300

    Updated Sliding Animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-const nextButton = document.querySelector(".btn");
-
 const mainContainer = document.querySelector("#main-container");
-nextButton.addEventListener("click", () => {
+
+function slideAnimation() {
     mainContainer.style.animationName = "sliding";
     setTimeout(() => {
         mainContainer.style.animationName = "";
     }, 1000);
-});
+}
